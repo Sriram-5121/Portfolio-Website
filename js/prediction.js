be4c57724780c59ab4b6dedd0531e72f052c03bd
Chinnasamy <sriram.chinnasamy@capgemini.com>
2024-08-04 16:34:52 +0530

    updated code

--- js/prediction.js
+++ js/prediction.js
@@ -22,7 +22,7 @@
 
             try {
                 // Make the API call
-                const response = await fetch('ipl-score-prediction.azurewebsites.net/docs', {
+                const response = await fetch('https://ipl-score-prediction.azurewebsites.net/docs', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
